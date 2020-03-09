38ef74df59b78ecc4a46a9cf08fcefa6e4faf5e3
Chance Smith <chancesmithb@gmail.com>
2020-03-09 09:01:08 -0500

    📦  NEW: wip Date issue

--- src/Greeter/Greeter.js
+++ src/Greeter/Greeter.js
@@ -1,9 +1,22 @@
-class Greeter{
-    consructor() {}
-    greet(person = "") {
-        return `hello${person !== '' ? ' ' + person: ''}!`
+class Greeter {
+  consructor(millisecondsFnc) {
+    this.currentDate = new Date(millisecondsFnc);
+  }
+  greet(person = "") {
+    if (this.currentDate.getHours() >= 6 && this.currentDate.getHours() <= 12) {
+      return `Good morning${
+        person !== ""
+          ? " " + person.trim().replace(/^\w/, c => c.toUpperCase())
+          : ""
+      }`;
+    } else {
+      return `Hello${
+        person !== ""
+          ? " " + person.trim().replace(/^\w/, c => c.toUpperCase())
+          : ""
+      }`;
     }
-
+  }
 }
 
-module.exports = Greeter
+module.exports = Greeter;
--- src/Greeter/__tests__/Greeter.test.js
+++ src/Greeter/__tests__/Greeter.test.js
@@ -1,17 +1,38 @@
-const Greeter = require('../Greeter');
+const Greeter = require("../Greeter");
 
-describe('Greeter', ()=>{  
-    let grt
-    beforeEach ( () => {
-        grt = new Greeter()
-    })
+describe("Greeter", () => {
+  let grt;
+  let currentDate;
 
-    test("should say hello", () => {
-        const result = grt.greet()
-        expect(result).toEqual("hello!") 
-    })
-    test("should say hello to person", () =>  {
-        const result = grt.greet("Jeremy")
-        expect(result).toEqual("hello Jeremy!") 
-    })
-})
+  beforeEach(() => {
+    currentDate = new Date();
+    currentDate.setHours(7);
+    currentDate.setMinutes(0);
+    grt = new Greeter(currentDate);
+  });
+
+  test("should say hello", () => {
+    const result = grt.greet();
+    expect(result).toEqual("Hello");
+  });
+
+  test("should say hello to person", () => {
+    const result = grt.greet("Jeremy");
+    expect(result).toEqual("Hello Jeremy");
+  });
+
+  test("should trim extra spaces", () => {
+    const result = grt.greet("    Jeremy  ");
+    expect(result).toEqual("Hello Jeremy");
+  });
+
+  test("Should capitalize the first letter of the name", () => {
+    const result = grt.greet("jeremy");
+    expect(result).toEqual("Hello Jeremy");
+  });
+
+  test("should say good morning", () => {
+    const result = grt.greet("jeremy");
+    expect(result).toEqual("Good Morning Jeremy");
+  });
+});
